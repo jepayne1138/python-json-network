756417333b3394e84eb50161367153018c47cf5a
james.payne <jepayne1138@gmail.com>
2015-11-06 17:11:49 -0600

    Backlog option listen() method and argument parsing

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python3
 
+import argparse
 import socket
 import sys
 import os
@@ -8,12 +9,12 @@ from contextlib import closing
 from typing import *
 
 
-def run_server(address: str, port: int, chunksize: int, out_dir: str=None):
+def run_server(address: str, port: int, chunksize: int, out_dir: str=None, backlog: int=None):
     out_dir = os.getcwd() if out_dir is None else out_dir
 
     with socket.socket() as sock:  # type: socket.socket
         sock.bind((address, port))
-        sock.listen()
+        sock.listen() if backlog is None else sock.listen(backlog)
 
         while True:
             conn, _ = sock.accept()  # type: socket.socket, (str, int)
@@ -29,10 +30,34 @@ def run_server(address: str, port: int, chunksize: int, out_dir: str=None):
 
 
 def main():
-    ADDRESS = 'localhost'
-    PORT = 13579
-    CHUNKSIZE = 1024
-    run_server(ADDRESS, PORT, CHUNKSIZE)
+    # Parse arguments
+    parser = argparse.ArgumentParser(
+        description='Runs a server that copies received data to a new file.'
+    )
+    parser.add_argument('-a', '--address',
+        default='localhost', type=str,
+        help='Server address'
+    )
+    parser.add_argument('-p', '--port',
+        default=13579, type=int,
+        help='Port number'
+    )
+    parser.add_argument('-c', '--chunksize',
+        default=1024, type=int,
+        help='Chunk size to read in from connections'
+    )
+    parser.add_argument('-d', '--directory',
+        default=os.getcwd(), type=str,
+        help='Directory where received files should be written to'
+    )
+    parser.add_argument('-b', '--backlog',
+        default=None, type=int,
+        help='Number of unaccepted connections to buffer'
+    )
+    args = parser.parse_args()
+
+    run_server(args.address, args.port, args.chunksize, args.directory, args.backlog)
+
 
 if __name__ == '__main__':
     main()
